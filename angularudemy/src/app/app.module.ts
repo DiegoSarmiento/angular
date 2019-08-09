2fbc6cf15ccf56d15990d35b75d5986694e36b5e
Diego <jd.sarmientoh.7@gmail.com>
2019-08-09 10:23:12 -0500

    Version 0.0.1

--- angularudemy/src/app/app.module.ts
+++ angularudemy/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrutaComponent,
+    EmpleadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
