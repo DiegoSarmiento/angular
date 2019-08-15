1a25ed6a4494cf7190e35683e9ac7bb37c3b915f
Diego <jd.sarmientoh.7@gmail.com>
2019-08-15 11:08:41 -0500

    navegacion

--- angularudemy/src/app/app.routing.ts
+++ angularudemy/src/app/app.routing.ts
@@ -4,12 +4,16 @@ import { Routes, RouterModule } from "@angular/router";
 // Importar Componentes
 import { EmpleadoComponent } from "./empleado/empleado.component";
 import { FrutaComponent } from "./fruta/fruta.component";
+import { ContactoComponent } from "./contacto/contacto.component";
+import { HomeComponent } from "./home/home.component";
 
 const appRoutes: Routes = [
     {path: '', component: EmpleadoComponent},
     {path: 'empleado', component: EmpleadoComponent},
     {path: 'fruta', component: FrutaComponent},
-    {path: '**', component: FrutaComponent},
+    {path: 'pagina-principal', component: HomeComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: '**', component: HomeComponent},
 ];
 
 export const appRoutingProviders:any[] = [];
