694a5c1430577c01246e6a6d73c1fbc084bebfa8
Diego <jd.sarmientoh.7@gmail.com>
2019-08-16 18:06:14 -0500

    formularios 0.0.2

---
+++ angularudemy/src/app/coches/coches.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Coche } from "./coche";
+
+@Component({
+  selector: 'app-coches',
+  templateUrl: './coches.component.html',
+  styleUrls: ['./coches.component.css']
+})
+export class CochesComponent implements OnInit {
+
+  public coche : Coche;
+
+  constructor() { 
+    this.coche = new Coche("","","");
+  }
+
+  onSubmit(){
+    console.log(this.coche);
+  }
+
+  ngOnInit() {
+  }
+
+}
