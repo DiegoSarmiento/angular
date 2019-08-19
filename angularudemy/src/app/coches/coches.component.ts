e3c040eb85cab3a91236187e6774a92706e0d708
Diego <jd.sarmientoh.7@gmail.com>
2019-08-18 22:45:32 -0500

    fomrulario 1.0

--- angularudemy/src/app/coches/coches.component.ts
+++ angularudemy/src/app/coches/coches.component.ts
@@ -10,9 +10,13 @@ import { Coche } from "./coche";
 export class CochesComponent implements OnInit {
 
   public coche : Coche;
-
+  public coches:Array<Coche>;
   constructor() { 
     this.coche = new Coche("","","");
+    this.coches = [
+      new Coche("Seat Panda", "120", "Blanco"),
+      new Coche("Renault Clio", "110", "Azul"),
+    ];
   }
 
   onSubmit(){
